2d2e6e6fffc3f6f7c0291d68bdcc84b928fe8666
Juan Formoso <trajano.juan@gmail.com>
2022-02-11 20:51:10 -0300

    Tentando corrigir erro no app hawkins

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = false;
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
